707dd0a61e84e35897f28188b6f7d18c0cc9a0fd
lukejohnsen <johnsen.luke@gmail.com>
2022-09-01 21:30:26 -0700

    templates branch and adding attractions template

---
+++ template-parts/template-attractions.php
@@ -0,0 +1,162 @@
+<?php /* Template Name: Attractions */ ?>
+<?php  get_header(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/attractions.css" />
+</head>
+
+<body>
+    <div class="attractions">
+        <div class="video-hero">
+            <h2 class="hero-heading">Attractions</h2>
+            <p class="hero-text">With all new attractions, updated scenes, more scares and a new location closer to town
+                on Tangerine & I-10, Terror In the Corn is gearing up for the greatest, most ghastly Halloween yet!</p>
+        </div>
+
+        <div class="get-tickets-button">
+            <a class='get-tickets-text'>Get Tickets</a>
+        </div>
+
+
+        <div class="field-of-screams-container">
+            <div class="border-container">
+            <div class="inner-hero">
+                <h2 class="attraction-heading">Field of Screams</h2>
+                <p class="attraction-text">See what dangers and devilish scares await you in the darkness of a
+                    haunted
+                    cornfield. Avoid the insane locals who lurk in the long grass. Escape the monsters of lore just
+                    waiting to snatch their next victim. Keep away from the creatures that call the corn home. Enter
+                    if
+                    you dare, but remember…if you get lost in the Field of Screams, you may never make it out again!
+                </p>
+
+                <div class="get-tickets-button-alt">
+                    <a class='get-tickets-text-alt'>Get Tickets</a>
+                </div>
+            </div>
+        </div>
+        </div>
+
+        <div class="shady-acres-asylum-container">
+            <div class="border-container-alt">
+                <div class="inner-hero">
+                    <h2 class="attraction-heading">Shady Acres Asylum</h2>
+                    <p class="attraction-text">Explore our insanitorium where unthinkable events occurred and the
+                        scariest
+                        secrets are still waiting to be fully uncovered. As you wander the Shady Acres Asylum, you’ll be
+                        surrounded by the same darkness, death and decay that became the final resting place for the
+                        criminally-insane. Will you survive or, like the previous “patients” who lost their minds, will
+                        you
+                        succumb to its insanity?</p>
+
+                    <div class="get-tickets-button-alt">
+                        <a class='get-tickets-text-alt'>Get Tickets</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="circus-of-horrors-container">
+            <div class="border-container">
+                <div class="inner-hero">
+                    <h2 class="attraction-heading">Circus of Horrors</h2>
+                    <p class="attraction-text">Explore our insanitorium where unthinkable events occurred and the
+                        scariest
+                        secrets are still waiting to be fully uncovered. As you wander the Shady Acres Asylum, you’ll be
+                        surrounded by the same darkness, death and decay that became the final resting place for the
+                        criminally-insane. Will you survive or, like the previous “patients” who lost their minds, will
+                        you
+                        succumb to its insanity?</p>
+
+                    <div class="get-tickets-button-alt">
+                        <a class='get-tickets-text-alt'>Get Tickets</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="axe-throwing-container">
+            <div class="border-container-alt">
+                <div class="inner-hero">
+                    <h2 class="attraction-heading">Axe Throwing</h2>
+                    <p class="attraction-text">Where else are you actually encouraged to play with sharp objects? Just
+                        like
+                        darts, only better, you can let your wild side out and axe-perience the rush of axe throwing!
+                        It’s a
+                        rush of adrenaline when you hit a bullseye and hear your friends screaming your name. Come split
+                        some timber at Terror In the Corn. (Not included in price; CLICK HERE to purchase Axe Throwing
+                        tickets in advance.)</p>
+
+                    <div class="get-tickets-button-alt">
+                        <a class='get-tickets-text-alt'>Get Tickets</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="escape-room-container">
+            <div class="border-container">
+                <div class="inner-hero">
+                    <h2 class="attraction-heading">Escape Room</h2>
+                    <p class="attraction-text">Can you make it out alive before your time runs out? With only minutes
+                        before
+                        the murderer strikes again, try to escape our scary school bus and avoid the serial killer who
+                        wants
+                        to make you their next victim. Grab some friends, figure out the clues, and find your way off
+                        the
+                        school bus from hell before you’re stuck inside…forever. (Not included in price; CLICK HERE to
+                        purchase Escape Room tickets in advance.)</p>
+
+                    <div class="get-tickets-button-alt">
+                        <a class='get-tickets-text-alt'>Get Tickets</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="zombie-paintball-container">
+            <div class="border-container-alt">
+                <div class="inner-hero" id="zb-hero">
+                    <h2 class="attraction-heading">Zombie Paintball Shootout</h2>
+                    <p class="attraction-text">Shoot live zombies! Can you handle the walking dead before they attack?
+                        With
+                        18 mounted paintball guns aimed at a field of both fixed targets and live actors who slowly make
+                        their way towards you, jump out from behind obstacles and taunt those hunting them, see if you
+                        have
+                        what it takes to “take out” the zombies before they turn you into one! (Not included in price;
+                        CLICK
+                        HERE to purchase Zombie Paintball tickets in advance.)</p>
+
+                    <div class="get-tickets-button-alt">
+                        <a class='get-tickets-text-alt'>Get Tickets</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="midway-container">
+            <div class="border-container">
+            <div class="inner-hero">
+                <h2 class="attraction-heading">Midway Area</h2>
+                <p class="attraction-text">In addition to the three haunted attractions included in your ticket price,
+                    plus the Zombie Paintball, Escape Room, and Ax Throwing (for additional fees), there’s much more to
+                    see and do at Terror in the Corn.</p>
+
+                <div class="get-tickets-button-alt">
+                    <a class='get-tickets-text-alt'>Learn More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
+</body>
+
+</html>
